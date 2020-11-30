e3bc78bfc770d4a7a097e5ac5f7c1c95469960ef
uchiborik <uk.22.uchi@gmail.com>
2020-12-01 00:27:59 +0900

    Update DemoToken.sol

--- DemoToken.sol
+++ DemoToken.sol
@@ -5,10 +5,10 @@ pragma solidity >=0.4.22 <0.7.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 // デモ用トークン
 contract DemoToken is ERC20{
-  uint public INITIAL_SUPPLY = 10000;
-  uint public AMOUNT = 100;
+  uint INITIAL_SUPPLY = 10000;
+  uint AMOUNT = 100;
   constructor() public ERC20("DemoToken", "DT"){
-    _mint(msg.sender, INITIAL_SUPPLY);  // コントラクト発行アカウント
+    _mint(msg.sender, INITIAL_SUPPLY);  // コントラクト発行アカウント(Remix 1番目のアカウント)
     _transfer(msg.sender,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, AMOUNT);  // ユーザA (Remix 2番目のアカウント)
     _transfer(msg.sender,0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, AMOUNT);  // ユーザB (Remix 3番目のアカウント)
   }
